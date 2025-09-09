834f45355f8ebbdaca4f3b088f575dbb12dd1b4f
hillarys Camilo <Hcamilo@ssv.local>
2025-09-09 17:58:04 -0400

    dg

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "../features/home/header/header.component";
+
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
